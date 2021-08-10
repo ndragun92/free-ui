ca59d1097c6ac711d1d92e01341feb150884ee1d
ndragun <39500267+ndragun92@users.noreply.github.com>
2021-08-10 22:33:48 +0200

    feat: work on layout

--- components/Internal/Navbar/InternalNavbar.vue
+++ components/Internal/Navbar/InternalNavbar.vue
@@ -1,6 +1,100 @@
 <template>
-  <div>
-    <nav id="main">Main nav</nav>
-    <div id="subnav">Subnav</div>
+  <div class="nav">
+    <nav class="nav__main">
+      <div class="nav__main-container">
+        <div class="nav__main-logo">FREE<span>UI</span></div>
+        <div>
+          <ul class="nav__main-links">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Docs</a>
+            </li>
+            <li>
+              <a href="#">Examples</a>
+            </li>
+            <li>
+              <a href="#">Releases</a>
+            </li>
+            <li>
+              <a href="#">Download</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="nav__sub">
+      <div class="nav__sub-container">
+        <div class="nav__sub-search">
+          <input type="search" placeholder="Search docs..." />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+@mixin navContainer {
+  padding: 0 20px;
+  max-width: 1400px;
+  width: 100%;
+  margin: 0 auto;
+}
+.nav {
+  &__main {
+    background-color: var(--freeui-color-gray-50);
+    border-bottom: 1px solid var(--freeui-color-gray-200);
+    height: 60px;
+    &-container {
+      @include navContainer;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      height: 100%;
+    }
+    &-logo {
+      font-size: 2.4rem;
+      span {
+        font-weight: 600;
+        color: dodgerblue;
+      }
+    }
+    &-links {
+      display: flex;
+      gap: 15px;
+      list-style: none;
+      li {
+        a {
+          text-decoration: none;
+          color: var(--freeui-color-gray-900);
+        }
+      }
+    }
+  }
+  &__sub {
+    background-color: var(--freeui-color-gray-100);
+    border-bottom: 1px solid var(--freeui-color-gray-200);
+    height: 40px;
+    &-container {
+      @include navContainer;
+      display: flex;
+      align-items: center;
+      height: 100%;
+    }
+    &-search {
+      width: 100%;
+      input {
+        border: none;
+        background: transparent;
+        height: 30px;
+        width: 100%;
+        max-width: 300px;
+        &:focus {
+          outline: none;
+        }
+      }
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
+  <div class="layout">
     <InternalNavbar />
-    <Nuxt />
+    <main class="main">
+      <aside class="aside">Aside</aside>
+      <div class="root">
+        <Nuxt />
+      </div>
+    </main>
   </div>
 </template>
 <script>
@@ -14,5 +19,39 @@ export default class LayoutDefault extends Vue {}
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
 body {
   font-family: 'Roboto', Arial, Helvetica, sans-serif;
+}
+#__nuxt,
+#__layout,
+#__layout > div {
+  height: 100%;
+}
+</style>
+
+<style lang="scss" scoped>
+@mixin mainContainer {
+  padding: 0 20px;
+  width: 100%;
+  max-width: 1400px;
+  margin: 0 auto;
+}
+.layout {
+  display: flex;
+  flex-direction: column;
+}
+.main {
+  @include mainContainer;
+  display: flex;
+  gap: 20px;
+  height: 100%;
+}
+.aside {
+  width: 300px;
+  border-right: 1px solid var(--freeui-color-gray-100);
+  padding: 20px 0;
+}
+.root {
+  flex-grow: 1;
+  min-width: 0;
+  padding: 20px 0;
 }
 </style>
