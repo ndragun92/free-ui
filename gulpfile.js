09f039a8c040d34adc9be84a192da10fb268d6b1
ndragun <39500267+ndragun92@users.noreply.github.com>
2021-08-10 20:04:53 +0200

    feat: define css structure and set up gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const sourcemaps = require('gulp-sourcemaps')
+const sassImportOnce = require('gulp-sass-import-once')
 const sassPath = './assets/styles/sass/**/*.scss'
 
 function buildStyles() {
   return gulp
     .src(sassPath)
+    .pipe(sassImportOnce())
     .pipe(sourcemaps.init())
     .pipe(sass())
     .on('error', sass.logError)
