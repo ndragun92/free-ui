adb233d6a0192bdf51bb205c7a44ae21835c6581
ndragun <39500267+ndragun92@users.noreply.github.com>
2021-09-02 13:13:15 +0200

    feat: new structure for writing examples

---
+++ components/External/Elements/Alerts/FreeAlert/FreeUiAlert.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <free-ui-alert-basic />
+    <free-ui-alert-icon />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import FreeUiAlertBasic from '~/components/External/Elements/Alerts/FreeAlert/Basic/FreeUiAlertBasic.vue'
+import FreeUiAlertIcon from '~/components/External/Elements/Alerts/FreeAlert/Icon/FreeUiAlertIcon.vue'
+@Component({
+  components: { FreeUiAlertIcon, FreeUiAlertBasic },
+})
+export default class FreeUiAlert extends Vue {}
+</script>
